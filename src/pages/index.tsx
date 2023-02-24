80726be0770dc369e2a540b3097a994d0aae5dd7
Pablo Misael Peloc <pmpeloc@gmail.com>
2023-02-24 00:16:31 -0300

    feat: getServerSideProps added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
+import { Categoria, PrismaClient } from '@prisma/client';
+
+interface Props {
+  categorias: Categoria[];
+}
+
+export default function Home({ categorias }: Props) {
+  console.table(categorias);
   return <h1>Hola</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categorias = await prisma.categoria.findMany();
+
+  return { props: { categorias } };
+};
